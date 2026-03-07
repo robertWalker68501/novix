05bdf3b519fb99ecb38840bc01cc6fde9aee69e4
robert <robert.walker6580@gmail.com>
2026-03-07 10:33:52 -0800

    01: project setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 const Home = () => {
   return (
     <div className=''>
-      <h1 className=''>Home Page</h1>
+      <Button>Click Me!</Button>
     </div>
   );
 };
