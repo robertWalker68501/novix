336273470f5a9a86e8f7e7fdb6309c013b296822
Robert Walker <robert.walker6580@gmail.com>
2026-03-09 12:32:35 -0700

    07: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,7 @@
-'use client';
-
-import { useMutation, useQuery } from 'convex/react';
-
-import { Button } from '@/components/ui/button';
-
-import { api } from '../../convex/_generated/api';
+import ProjectsView from '@/features/projects/components/projects-view';
 
 const Home = () => {
-  const projects = useQuery(api.projects.get);
-  const createProject = useMutation(api.projects.create);
-
-  return (
-    <main className='flex flex-col gap-2 p-4'>
-      <Button
-        onClick={() =>
-          createProject({
-            name: 'New Project123',
-          })
-        }
-      >
-        Add New
-      </Button>
-      {projects?.map((project) => (
-        <div
-          key={project._id}
-          className='flex flex-col rounded border p-2'
-        >
-          <p>{project.name}</p>
-          <p>OwnerId: {project.ownerId}</p>
-        </div>
-      ))}
-    </main>
-  );
+  return <ProjectsView />;
 };
 
 export default Home;
